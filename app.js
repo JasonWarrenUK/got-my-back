eefda97f73db890d097cce26ef95a951ac5cce06
Jason Warren <contact.jwarren@gmail.com>
2024-06-03 19:21:11 +0100

    Render user input form with pug

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 //h1 Setup
+//h2 Declarations
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+//h2 Routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+//h2 Express
 const app = express();
 
-//h2 View Engine
+//h1 Pug
+//h3 Pug Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//h3 Pug Utils
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//h1 Routes
+//h2 Content Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+//h2 Safety Routes
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

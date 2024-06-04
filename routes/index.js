c6d7504c815abb0e795011bcbc0326b3c4da58b9
Jason Warren <contact.jwarren@gmail.com>
2024-06-04 17:17:22 +0100

    App Navigation Add separate `.pug` files for different views Fixes #9

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Got My Back' });
 });
 
-router.post('/', function (req, res, next) {
+router.post('/list', function (req, res, next) {
   const postcode = req.body.postcode;
   const topic = req.body.topic;
 
   console.log(`Postcode: ${postcode}`);
   console.log(`Topic: ${topic}`);
 
-  res.render('index', { title: 'Got My Back' });
+  res.render('list', { title: 'Got My Back' });
+});
+
+router.get('/list', function (req, res, next) {
+  res.render('list', { title: 'Got My Back' });
+});
+
+router.get('/entry', function (req, res, next) {
+  res.render('entry', { title: 'Got My Back' });
 });
 
 module.exports = router;

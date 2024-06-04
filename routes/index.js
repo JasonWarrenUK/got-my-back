6db2ec1479c2284de50dc8a7b69d2d6b02b751a3
Jason Warren <contact.jwarren@gmail.com>
2024-06-04 16:13:31 +0100

    `views/index.pug` - add POST method to form `routes/index.js` - add post router that logs input Fixes #7

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Got My Back' });
+});
+
+router.post('/', function (req, res, next) {
+  const postcode = req.body.postcode;
+  const topic = req.body.topic;
+
+  console.log(`Postcode: ${postcode}`);
+  console.log(`Topic: ${topic}`);
+
   res.render('index', { title: 'Got My Back' });
 });
 

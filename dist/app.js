b9ba1dfbec35ea8354b89a8e982275a717bbcb2e
Jason Warren <contact.jwarren@gmail.com>
2024-06-11 15:47:35 +0100

    is now ts-first

--- dist/app.js
+++ dist/app.js
@@ -8,40 +8,33 @@ import { dirname } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import routes from "./routes/rIndex.js";
-
 //h1 Setup
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 //?? Wut?
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 //h1 Content
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(routes);
-
 //h1 Error Handling
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 app.use(function (err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  res.status(err.status || 500);
-  res.render("error");
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
 });
-
 //h1 Listen
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ dist/routes/rIndex.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import results from './rResults.js';
+const router = express.Router();
+router.use('/', results);
+export default router;
